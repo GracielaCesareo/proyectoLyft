f93c3a3a9b136c6821e84ad09223f1ce73d25fdd
Gaby Trejo <gabytrejo303@gmail.com>
2017-04-26 16:58:19 -0500

    Logo blanco agregado a nav

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -1,11 +1,16 @@
 var navBar = document.getElementById("navBar");
+var logo = document.getElementsByClassName("logo");
+// 
+// var logoWhithe= document.createElement("img").scr ="assets/img/logo-white.png";
+//
+// logo.appendChild(logoWhithe);
+
 document.addEventListener("scroll", navStyleEdit);
 
 function navStyleEdit(){
   if (window.scrollY > 100) {
     navBar.style.backgroundColor = "white";
     navBar.style.color = "#333447";
-    nav
   } else {
     navBar.style.backgroundColor = "transparent";
     navBar.style.color = "whitesmoke";
